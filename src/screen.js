d492f7812a35c30cec1f44ec2376d75aa70245de
Adam C Hamlin <achamlin@gmail.com>
2019-11-22 20:45:05 -0500

    Stop setting terminal title

--- src/screen.js
+++ src/screen.js
@@ -4,6 +4,5 @@ const blessed = require('blessed');
 
 // Want this available globally for rendering
 module.exports = blessed.screen({
-    smartCSR: true,
-    title: 'FRAP'
+    smartCSR: true
 });
